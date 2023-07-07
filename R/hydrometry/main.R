d3a8853d2aabd7946fd16f825b6638cee6852ccf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-07 17:14:19 +0100

    Update R/hydrometry/main.R

--- R/hydrometry/main.R
+++ R/hydrometry/main.R
@@ -49,21 +49,11 @@ frame <- GetGazetteer(utm = utm)
 
 
 
-#' Intersections
+#' Deduce District Membership
 #'
 #'
-source(file = file.path(getwd(), 'R', 'hydrometry', 'Intersections.R'))
-cores <- parallel::detectCores() - 2
-doParallel::registerDoParallel(cores = cores)
-clusters <- parallel::makeCluster(cores)
-X <- parallel::clusterMap(clusters, fun = Intersections, districts, MoreArgs = list(frame = frame, utm = utm))
-parallel::stopCluster(clusters)
-rm(clusters, cores)
-
-collection <- X %>%
-  purrr::reduce(full_join, by='station_id')
-
-computations <- dplyr::left_join(x = frame, y = collection, by = 'station_id')
+source(file = file.path(getwd(), 'R', 'hydrometry', 'DeduceDistrictMembership.R'))
+computations <- DeduceDistrictMembership(districts = districts, frame = frame, utm = utm)
 
 
 
